3c00a618afb1379e1b22bf14dd54d9c6bf5f9d5d
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-08-30 22:41:27 +0600

    working on navbar

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,5 +1,15 @@
 import { ReactNode } from "react";
+import Footer from "../../components/shared/footer/Footer";
+import Navbar from "../../components/shared/navigation/Navbar";
 
 export default function layout({ children }: { children: ReactNode }) {
-  return <>{children}</>;
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 }
