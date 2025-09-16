c97106a86404fe22408128e12a1e927f47ab2d84
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-09-16 14:47:04 +0600

    mobile filter , scrollable

--- src/components/main/jobs/Sidebar.tsx
+++ src/components/main/jobs/Sidebar.tsx
@@ -10,6 +10,7 @@ import {
 } from "@/components/ui/sheet";
 import { Filter } from "lucide-react";
 import { useState } from "react";
+import { ScrollArea } from "../../ui/scroll-area";
 import SidebarFilter from "./SidebarFilter";
 
 interface FilterState {
@@ -32,22 +33,27 @@ const Sidebar = ({ onFiltersChange }: MobileFilterDrawerProps) => {
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetTrigger asChild>
-        <Button variant="outline" className="bg-transparent md:hidden">
+        <Button
+          variant="outline"
+          className="rounded-full bg-transparent md:hidden"
+        >
           <Filter className="mr-2 h-4 w-4" />
           Filters
         </Button>
       </SheetTrigger>
-      <SheetContent side="left" className="w-80 p-0">
+      <SheetContent side="left" className="w-full p-0">
         <SheetHeader className="p-6 pb-0">
           <SheetTitle>Filter Jobs</SheetTitle>
         </SheetHeader>
-        <div className="p-6 pt-0">
-          <SidebarFilter
-            onFiltersChange={(filters) => {
-              onFiltersChange(filters);
-              setOpen(false);
-            }}
-          />
+        <div className="pt-0">
+          <ScrollArea className="h-[87dvh]">
+            <SidebarFilter
+              onFiltersChange={(filters) => {
+                onFiltersChange(filters);
+                setOpen(false);
+              }}
+            />
+          </ScrollArea>
         </div>
       </SheetContent>
     </Sheet>
