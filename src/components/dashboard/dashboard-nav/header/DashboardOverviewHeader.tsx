a7adea1cf08c320c312daf9a94e0d84de90b3729
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-10-05 15:34:20 +0600

    minor change

--- src/components/dashboard/dashboard-nav/header/DashboardOverviewHeader.tsx
+++ src/components/dashboard/dashboard-nav/header/DashboardOverviewHeader.tsx
@@ -8,7 +8,7 @@ const DashboardOverviewHeader = ({ companyData }: { companyData: any }) => {
     <header className="bg-card sticky top-0 border-b">
       <div className="container mx-auto flex h-18 items-center justify-between px-6">
         <div className="flex min-w-0 flex-1 items-center space-x-3 sm:space-x-4">
-          <Avatar className="bg-card h-10 w-10 flex-shrink-0 rounded-full sm:h-12 sm:w-12">
+          <Avatar className="bg-primary h-10 w-10 flex-shrink-0 rounded-full sm:h-12 sm:w-12">
             <AvatarImage
               src={companyData.logo || "/placeholder.svg"}
               alt={companyData.name}
