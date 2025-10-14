87ce0f8d40d51801c5dc4b59d05d1dd3d4603654
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-10-14 16:17:25 +0600

    some empty page added

---
+++ src/app/(dashboard)/dashboard/analytics/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <h1 className="text-primary text-lg sm:text-2xl">Coming Soon</h1>
+    </div>
+  );
+};
+
+export default page;
---
+++ src/app/(dashboard)/dashboard/post-job/page.tsx
@@ -0,0 +1,9 @@
+const page = () => {
+  return (
+    <div className="flex h-screen w-full items-center justify-center">
+      <h1 className="text-lg text-pink-500 sm:text-2xl">Coming Soon</h1>
+    </div>
+  );
+};
+
+export default page;
