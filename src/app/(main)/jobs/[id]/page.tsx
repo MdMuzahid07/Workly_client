ed071edb37d220f9ba0df0a43d31520b8b0277a7
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-10-15 15:11:50 +0600

    jobs, jobdetails page seo

--- src/app/(main)/jobs/[id]/page.tsx
+++ src/app/(main)/jobs/[id]/page.tsx
@@ -1,4 +1,9 @@
+import { Metadata } from "next";
 import JobDetailsView from "../../../../view/job/JobDetailsView";
+
+export const metadata: Metadata = {
+  title: "Job Details",
+};
 
 const page = () => {
   return (
