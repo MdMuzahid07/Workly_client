964c0585fd83ac8ba661b1374fb1c7d83319eac4
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-10-17 23:48:40 +0600

    some ui improvement

--- src/app/(dashboard)/dashboard/post-job/page.tsx
+++ src/app/(dashboard)/dashboard/post-job/page.tsx
@@ -3,7 +3,9 @@ import CreateNewJobForm from "../../../../components/dashboard/job/CreateNewJobF
 const page = () => {
   return (
     <div className="container mx-auto space-y-6 px-4 py-6 sm:px-6 sm:py-8">
-      <CreateNewJobForm />
+      <div className="bg-card rounded-2xl p-6 md:p-10">
+        <CreateNewJobForm />
+      </div>
     </div>
   );
 };
