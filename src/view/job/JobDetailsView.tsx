1e84e0d68e575f4c411bbe260456c2dc6f51ce29
MdMuzahid07 <mdmuzahid7396@gmail.com>
2025-10-21 17:11:52 +0600

    minor update

--- src/view/job/JobDetailsView.tsx
+++ src/view/job/JobDetailsView.tsx
@@ -231,7 +231,7 @@ const JobDetailsView = () => {
                   <ul className="space-y-3">
                     {job.Benefits.map((benefit: any) => (
                       <li key={benefit.id} className="flex items-start">
-                        <span className="bg-primary/100 mt-2 mr-3 h-2 w-2 flex-shrink-0 rounded-full"></span>
+                        <span className="bg-primary mt-2 mr-3 h-2 w-2 rounded-full"></span>
                         <div className="flex-1">
                           <p className="text-foreground/90 font-medium">
                             {benefit.title}
