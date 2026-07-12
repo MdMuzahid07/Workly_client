a70de395837697fdf9115a585056ee2b98091051
MdMuzahid07 <mdmuzahid7396@gmail.com>
2026-07-12 10:49:11 +0600

    feat: notification, messaging sound, and job seeker, and employer seeing confirmation modal while click sign out from settings

--- src/redux/feature/notification/notificationSoundSlice.ts
+++ src/redux/feature/notification/notificationSoundSlice.ts
@@ -1,6 +1,6 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-interface NotificationSoundState {
+export interface NotificationSoundState {
   enabled: boolean;
   volume: number; // 0.0 to 1.0
 }
